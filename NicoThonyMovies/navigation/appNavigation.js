c6a8b13454065d03754962a2da722313a8f9d8b9
AnthonyRodrigues894 <anthonyserrano894@gmail.com>
2025-10-09 12:24:04 -0300

    criado homeScreen e appNavigation

---
+++ NicoThonyMovies/navigation/appNavigation.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {NavigationContainer, navigationcontainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function appNavigation() {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen
+                name = "home"
+                options={{ headerShown: false}}
+                component={HomeScreen}
+            />
+        </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
