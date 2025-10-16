6e17e1c31463911b5be30d6bead0190326f336e0
AnthonyRodrigues894 <anthonyserrano894@gmail.com>
2025-10-16 12:25:38 -0300

    recriando projeto

--- NicoThonyMovies/theme/index.js
+++ NicoThonyMovies/theme/index.js
@@ -1,4 +1,11 @@
+import { setStatusBarBackgroundColor } from "expo-status-bar"
+
+export const theme = {
+    background: '#eab308',
+    text: '#eab308',
+}
+
 export const styles = {
-    text: { color: '#eab308'},
-    background: { backgroundcolor: '#eab308'}
+    text: { color: theme.text },
+    background: { BackgroundColor: theme.background }
 }
