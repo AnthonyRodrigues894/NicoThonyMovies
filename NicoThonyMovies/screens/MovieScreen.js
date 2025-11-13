c9125952bf55ec474bda87fc3b4c84d93e68f2bb
AnthonyRodrigues894 <anthonyserrano894@gmail.com>
2025-11-13 12:15:21 -0300

    78 pag

--- NicoThonyMovies/screens/MovieScreen.js
+++ NicoThonyMovies/screens/MovieScreen.js
@@ -1,40 +1,63 @@
-import { View, Text, ScrollView, TouchableOpacity, Dimensions, Image } from 'react-native'
 import React, { useEffect, useState } from 'react'
+import { View, Text, ScrollView, TouchableOpacity, Dimensions, Image } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import { useNavigation, useRoute } from '@react-navigation/native'
-import { ChevronLeftIcon } from 'react-native-heroicons/outline'
-import { HeartIcon } from 'react-native-heroicons/solid'
-import { styles, theme } from '../theme'
-import { LinearGradient } from 'expo-linear-gradient'
+import { ChevronLeftIcon } from 'react-native-heroicons/outline';
+import { HeartIcon } from 'react-native-heroicons/solid';
+import { styles, theme } from '../theme';
+import { LinearGradient } from 'expo-linear-gradient';
 
-var {width, height } = Dimensions.get('window');
+var { width, height } = Dimensions.get('window');
+
 
 export default function MovieScreen() {
-    const { params: item } = useRoute();
-    const [isFavorite, toggleFavorite] = useState(false);
-    const navigation = useNavigation();
-    let movieName = "Joker: Folie à Deux";
-    useEffect(() => {
-        //call API
-    }, [item])
+  const { params: item } = useRoute();
+  const [isFavorite, toggleFavorite] = useState(false);
+  const [cast, setCast] = useState([1,2,3,4,5]);
+  const navigation = useNavigation();
+  let movieName = "Joker: Folie à Deux";
+  useEffect(() => {
+    // call API
+  }, [item])
 
   return (
     <ScrollView
-    contentContainerStyle={{ paddingBottom: 20, minHeight: '100%' }}
-    className="flex-1 bg-neutral-900"
+      contentContainerStyle={{ paddingBottom: 20, minHeight: '100%' }}
+      className="flex-1 bg-neutral-900"
     >
-        <View className="w-full">
-            <SafeAreaView className={"absolute z-20 w-full flex-row justify-betweem items-center p-4 pt-3"}>
-               <TouchableOpacity onPress={() => navigation.goBack()} style={styles.background} className="roubded=xl p-1">
-                <ChevronLeftIcon size="28" strokeWidth={2.5} color ="white" />
-                </TouchableOpacity> 
-                <TouchableOpacity onPress={() => toggleFavorite(! isFavorite)}>
-                    <HeartIcon size="36" color={isFavorite ? theme.background : "white"} />
-                </TouchableOpacity>
-            </SafeAreaView>
-        </View>
-            <Image
-              source={require('../assets/images/moviePoster2.jpg')}
+      <View className="w-full">
+        <SafeAreaView className={"absolute z-20 w-full flex-row justify-between items-center p-4 pt-3"}>
+          <TouchableOpacity onPress={() => navigation.goBack()} style={styles.background} className="rounded-xl p-1">
+            <ChevronLeftIcon size ="28" strokeWidth={2.5} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => toggleFavorite(!isFavorite)}>
+            <HeartIcon size="36" color={isFavorite ? theme.background: "white"} />
+          </TouchableOpacity>
+        </SafeAreaView>
 
+        {/*Generos */}
+        <View className="flex-row justify-center mx-4 space-x-2">
+          <Text className="text-neutral-400 front-semibold text-base text-center">
+            Drama 
+          </Text>
+          <Text className="text-neutral-400 font-semibold text-base text-center">
+            Crime
+          </Text>
+          <Text className="text-neutral-400 font-semibold text-base text-center">
+            Thriller
+          </Text>
+        </View>
+
+        {/*sinopse */}
+        <Text className="text-neutral-400 mx-4 tracking-wide">
+          Coringa 2 se passa depois dos acontecimentos do filme de 2019, após ser iniciado um movimento popular contra a 
+        </Text>
+
+        {/* Elenco */}
+        <Cast navigation={navigation} cast={cast} />
+
+       </View>
+      </View>
     </ScrollView>
   )
 }
